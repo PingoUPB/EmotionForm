4c4d06bc33fbc153ed383b671aeedd3b6d37c8dc
Michael Whittaker <post@michael-whittaker.de>
2013-04-08 14:17:10 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmotionForm::Application.config.secret_token = '196fbddfc1cb1653db9b4fe4aace4050b2dcf545080eb9cd0630537ec4424922e2f49220db91f738c33692205743808b399fca271fa788288dd9241ca39f4fed'
